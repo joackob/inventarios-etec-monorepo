ba62463f0266b3e0f02f9aeb351e6d15a41de8aa
Tobias Sanchez <tsanchezaguirre.18@etec.uba.ar>
2023-10-26 20:58:21 -0300

    agregue algunos objetos al getItemsMock

--- mobile/src/invetory/service/service-mock.ts
+++ mobile/src/invetory/service/service-mock.ts
@@ -9,6 +9,22 @@ export const getItemsMock = (): ItemInventory[] => {
       marca: "Description of Item 1",
       numero_de_serie: "fdfdfdfdfdfd",
     },
+    {
+      id: "2",
+      nombre: "Item 2",
+      marca: "Description of Item 2",
+      numero_de_serie: "fdfdfdfdfdfd",
+    },  {
+      id: "3",
+      nombre: "Item 3",
+      marca: "Description of Item  3",
+      numero_de_serie: "fdfdfdfdfdfd",
+    },  {
+      id: "4",
+      nombre: "Item 4",
+      marca: "Description of Item 4",
+      numero_de_serie: "fdfdfdfdfdfd",
+    },
   ];
 };
 
