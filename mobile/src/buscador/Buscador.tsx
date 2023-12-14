e3f0b3d1f0947e0ee8dad3b12c295570048fbeac
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-12-14 12:15:34 -0300

    agregue buscador basico

--- mobile/src/buscador/Buscador.tsx
+++ mobile/src/buscador/Buscador.tsx
@@ -1,20 +1,34 @@
-import 
-{ SafeAreaView }
- from "react-native-safe-area-context";
-import { Input } from "@rneui/base";
-import { View, ScrollView } from "react-native";
-import { SearchBar } from "react-native-screens";
-import { ItemCard } from "../inventario/screens/list-items/components";
+import { ScrollView, View, Text } from "react-native";
+import { SearchBar, useTheme } from "@rneui/themed";
+import tw from "twrnc";
+import { useState } from "react";
 
-const Login = () =>{
-return(
-   <SafeAreaView>
-      <SearchBar
-      placeholder="Buscar"
-      />
-  
-   </SafeAreaView>
- )
-}
+const Buscador = () => {
+  const { theme } = useTheme();
+  const [busqueda, setBusqueda] = useState("");
 
-export default Login;
+  return (
+    <ScrollView>
+      <View style={tw`m-4`}>
+        <SearchBar
+          placeholder="Buscar"
+          containerStyle={{
+            backgroundColor: theme.colors.white,
+            borderColor: theme.colors.white,
+            borderRadius: 30,
+          }}
+          inputContainerStyle={{ backgroundColor: theme.colors.white }}
+          inputStyle={{ color: theme.colors.black }}
+          onChangeText={(text: string) => setBusqueda(text)}
+          onClear={() => setBusqueda("")}
+          value={busqueda}
+        />
+      </View>
+      <View style={tw`m-4`}>
+        <Text>{busqueda}</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Buscador;
